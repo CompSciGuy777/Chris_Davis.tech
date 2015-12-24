dc5fc07510b317feaaca045956d64273171d7d27
Chris Davis <compsciguy777@gmail.com>
2015-12-24 21:55:57 +0900

    Built CRUD for Posts

--- Rails/blog/app/controllers/posts_controller.rb
+++ Rails/blog/app/controllers/posts_controller.rb
@@ -1,5 +1,49 @@
 class PostsController < ApplicationController
+	before_action :find_post, only: [:show, :edit, :update, :destroy]
+	def index
+	end
 
+	def new
+		@post = Post.new
+	end
 
+	def create
+		@post = Post.new post_params
 
+		if @post.save
+			redirect_to @post, notice: "Your Article was successfully created, would you like some coffee now, Chris?"
+		else
+			render 'new', notice: "Oh no, Chris, the article didn't save."
+		end
+	end
+
+	def show
+	end
+
+	def edit
+	end
+
+	def update
+		if @post.update post_params
+			redirect_to @post, notice: "Success"
+		else
+			render 'edit'
+		end
+	end
+
+	def destroy
+		@post.destroy
+		redirect_to posts_path
+	end
+
+	private
+
+	def post_params
+		params.require(:post).permit(:title, :content)
+	end
+
+	def find_post
+		@post = Post.find (params[:id])
+	end
 end
+
